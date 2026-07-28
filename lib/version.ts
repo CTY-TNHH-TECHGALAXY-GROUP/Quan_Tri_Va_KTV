215c4e37b26480fa2aa2de6e16e784735abf25e8
huynhngoctuanhieu2000-glitch <huynhngoctuanhieu2000-glitch>
2026-07-28 21:59:26 +0700

    chore: Cập nhật version

--- lib/version.ts
+++ lib/version.ts
@@ -1,3 +1,3 @@
 // Update khi có thay đổi quan trọng để force client reload
-export const APP_VERSION = '1.4.6';
-export const LAST_UPDATE = 'Khắc phục lỗi chênh lệch số lượng công việc (hiển thị sai task của phòng chung) và tự động dọn rác các task đã hủy gán trước đó.';
+export const APP_VERSION = '1.4.7';
+export const LAST_UPDATE = 'Fix logic tính tiền tua theo work_type của KTV ở Report và KTV Dashboard';
