e2c487777270bb8a0d34236995b22317c0d6f84a
huynhngoctuanhieu2000-glitch <huynhngoctuanhieu2000-glitch>
2026-07-28 14:36:51 +0700

    fix: sửa lỗi cú pháp version.ts

--- lib/version.ts
+++ lib/version.ts
@@ -1,3 +1,3 @@
 // Update khi có thay đổi quan trọng để force client reload
-export const ;
-export const Sửa thứ tự hiển thị Nhóm Việc trên App KTV và bổ sung hiển thị Công việc phòng;
+export const APP_VERSION = '1.3.5';
+export const LAST_UPDATE = 'Sửa thứ tự hiển thị Nhóm Việc trên App KTV và bổ sung hiển thị Công việc phòng';
