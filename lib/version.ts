947c40fdee3fcec6b33be50a84827305f74e72e8
huynhngoctuanhieu2000-glitch <huynhngoctuanhieu2000-glitch>
2026-08-03 21:21:03 +0700

    chore: bump version 1.7.47

--- lib/version.ts
+++ lib/version.ts
@@ -1,2 +1,2 @@
 export const APP_VERSION = '1.7.47';
-export const LAST_UPDATE = 'Sửa lỗi KTV B bị báo "đi trễ" và bắt nhập lý do khi báo cáo đến tiệm.';
+export const LAST_UPDATE = 'Tự động gán/xoá công việc cá nhân cho nhân viên khi Admin thêm/bớt ở Ma Trận Phòng.';
