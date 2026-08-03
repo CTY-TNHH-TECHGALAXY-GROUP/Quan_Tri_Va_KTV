fbd10527d3d56bca2d6fec64beebfd82850ca1dc
huynhngoctuanhieu2000-glitch <huynhngoctuanhieu2000-glitch>
2026-08-03 09:53:34 +0700

    chore: Cập nhật version lên 1.7.37

--- lib/version.ts
+++ lib/version.ts
@@ -1,2 +1,2 @@
-export const APP_VERSION = '1.7.35';
-export const LAST_UPDATE = 'Bỏ điều kiện duyệt phòng, KTV nhận được tiền tua ngay lập tức sau khi bàn giao';
+export const APP_VERSION = '1.7.37';
+export const LAST_UPDATE = 'Đồng bộ Ví và Lịch sử (Fix lỗi API lấy nhầm Type A thay vì Type B). Fix lỗi trừ tiền giặt đồ 2 lần.';
