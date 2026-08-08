aadece838f7b0a840edd4226a7365f28921786e2
huynhngoctuanhieu2000-glitch <huynhngoctuanhieu2000-glitch>
2026-08-08 16:28:36 +0700

    chore: bump version 1.2.68

--- lib/version.ts
+++ lib/version.ts
@@ -1,2 +1,2 @@
-export const APP_VERSION = '1.2.66';
-export const LAST_UPDATE = 'Fix lỗi cộng dồn thời gian và tự động gộp sai Dịch vụ 2';
+export const APP_VERSION = '1.2.68';
+export const LAST_UPDATE = 'Ẩn Nhà vệ sinh và Nhà tắm khỏi menu chọn phòng Bảng Điều Phối';
