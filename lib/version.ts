ecd1edff3bd3bc52757927f28d79ab6edf985ee0
huynhngoctuanhieu2000-glitch <huynhngoctuanhieu2000-glitch>
2026-08-20 21:30:44 +0700

    fix: sửa lỗi cú pháp thiếu dấu ngoặc kép trong file version.ts

--- lib/version.ts
+++ lib/version.ts
@@ -1,2 +1,2 @@
 ﻿export const APP_VERSION = '1.0.14';
-export const LAST_UPDATE = 'Sửa lỗi hiển thị sai Mã Chính trên bảng Dispatch (xóa bỏ hậu tố A/B)
+export const LAST_UPDATE = 'Sửa lỗi hiển thị sai Mã Chính trên bảng Dispatch (xóa bỏ hậu tố A/B)';
