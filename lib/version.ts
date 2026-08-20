b042d7ec0b6d226bc477bed45421d4a95189cd71
huynhngoctuanhieu2000-glitch <huynhngoctuanhieu2000-glitch>
2026-08-20 18:49:21 +0700

    chore: cập nhật version 1.0.41

--- lib/version.ts
+++ lib/version.ts
@@ -1,2 +1,2 @@
-export const APP_VERSION = '1.0.40';
-export const LAST_UPDATE = 'Bổ sung thêm ngày vào thời gian báo trước rút tiền KTV và sửa lỗi hiển thị';
+﻿export const APP_VERSION = '1.0.41';
+export const LAST_UPDATE = 'Sửa lỗi tính điểm bonus KTV: KTV phục vụ riêng khách sẽ nhận full 20đ, không bị chia đôi do đếm nhầm khách của người khác, và sửa lỗi phạt < 60 phút sai quy định.';
